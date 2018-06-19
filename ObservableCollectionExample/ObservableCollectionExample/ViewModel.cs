ca1d654cb81f6a2808b378719c42f2b58376468e
gjansen <geert__jansen@hotmail.com>
2018-06-19 17:12:37 +0200

    Add viewmodel

---
+++ ObservableCollectionExample/ObservableCollectionExample/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionExample
+{
+    public class ViewModel
+    {
+
+    }
+
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private string LocalName;
+        private string LocalTelephoneNumber;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name {
+            get => this.LocalName;
+            set
+            {
+                if (value != this.LocalName)
+                {
+                    this.LocalName = value;
+                    OnPropertyChanged(nameof(this.Name));
+                }
+            }
+        }
+
+        public string TelephoneNumber {
+            get => this.LocalTelephoneNumber;
+            set 
+            {
+                if (value != this.LocalTelephoneNumber)
+                {
+                    this.LocalTelephoneNumber = value;
+                    OnPropertyChanged(nameof(TelephoneNumber));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
